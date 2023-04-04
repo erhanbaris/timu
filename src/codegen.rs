84a83e91a11e0fd94789147986d34c27706fbbc5
Erhan BARIS <erhanbaris@gmail.com>
2023-04-04 17:31:46 +0200

    Ast changes

--- src/codegen.rs
+++ src/codegen.rs
@@ -1,21 +1,67 @@
-use inkwell::{context::Context, module::Module, builder::Builder};
+//use inkwell::{context::Context, module::{Module, Linkage}, builder::Builder, types::BasicMetadataTypeEnum, AddressSpace};
 
-use crate::ast::{FileAst, Ast};
+use crate::ast::{FileAst, BlockAst, FunctionDefinitionAst, FunctionCallAst, PrimativeAst, StatementAst};
 
-pub struct CodeGen<'ctx> {
-    pub context: &'ctx Context,
-    pub module: Module<'ctx>,
-    pub builder: Builder<'ctx>,
+pub struct CodeGen {
+
 }
 
-impl CodeGen<'_> {
+impl CodeGen {
     pub fn dump(&self) {
-        let _result = self.module.print_to_string();
-        println!(">> {}", _result);
+        
     }
 
-    pub fn compile(&self, ast: Box<FileAst>) {
-        ast.generate(self);
+    pub fn compile(&self, file: Box<FileAst>) {
+        file.generate(self);
     }
 
+}
+
+pub trait CompileTrait {
+    fn generate(&self, compile: &CodeGen);
+}
+
+impl CompileTrait for PrimativeAst {
+    fn generate(&self, compile: &CodeGen) {
+        
+    }
+}
+
+impl CompileTrait for FunctionCallAst {
+    fn generate(&self, compile: &CodeGen) {
+        
+ 
+    }
+}
+
+impl CompileTrait for FunctionDefinitionAst {
+    fn generate(&self, compile: &CodeGen) {
+        self.body.generate(compile);
+    }
+}
+
+impl CompileTrait for FileAst {
+    fn generate(&self, compile: &CodeGen) {
+        for function in self.functions.iter() {
+            function.generate(compile);
+        }
+    }
+}
+
+impl CompileTrait for BlockAst {
+    fn generate(&self, compile: &CodeGen) {
+        for statement in self.statements.iter() {
+            statement.generate(compile);
+        }
+    }
+}
+
+
+impl CompileTrait for StatementAst {
+    fn generate(&self, compile: &CodeGen) {
+        match self {
+            StatementAst::FunctionCall(func_call) => func_call.generate(compile),
+            StatementAst::Primative(primative) => primative.generate(compile)
+        }
+    }
 }
