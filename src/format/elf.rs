cb8e1067918b7c850493117cf7f12800434c1859
Erhan BARIS <erhanbaris@gmail.com>
2024-07-31 22:02:42 +0200

    Mach-O format

--- src/format/elf.rs
+++ src/format/elf.rs
@@ -1,24 +1,12 @@
 use core::str;
-use std::{ffi::CStr, fs};
+use std::fs;
+
+use crate::format::str_from_null_terminated_utf8;
+
+use super::{BitMode, BufferReader, Endianness, Size};
 
 
 /* Enums */
-#[derive(Debug, Default)]
-pub enum Endianness {
-    #[default]
-    Little,
-    Big,
-}
-
-#[repr(u8)]
-#[derive(Debug, Copy, Clone, Default, PartialEq)]
-pub enum BitMode {
-    #[default]
-    _32,
-    _64,
-}
-
-
 #[repr(u32)]
 #[derive(Debug, Default)]
 pub enum Segment {
@@ -42,25 +30,6 @@ pub enum ElfType {
     Executable = 0x02,
     Shared = 0x03,
     Core = 0x04,
-}
-
-#[repr(u16)]
-#[derive(Debug, Copy, Clone, Default)]
-pub enum Size {
-    #[default]
-    None = 0x00,
-    u32(u32),
-    u64(u64),
-}
-
-impl From<Size> for usize {
-    fn from(value: Size) -> Self {
-        match value {
-            Size::None => todo!("program_header_table_offset could not parsed"),
-            Size::u32(size) => size as usize,
-            Size::u64(size) => size as usize,
-        }
-    }
 }
 
 #[repr(u16)]
@@ -208,70 +177,6 @@ pub struct ElfFormat<'a> {
     pub section_headers: Vec<ElfSectionHeader<'a>>,
 }
 
-#[derive(Debug, Default)]
-pub struct BufferReader<'a> {
-    pub data: &'a [u8],
-    pub index: usize
-}
-
-impl<'a> BufferReader<'a> {
-    pub fn new(data: &'a [u8]) -> Self {
-        Self { data, index: 0 }
-    }
-
-    pub fn as_u8(&mut self) -> u8 {
-        let value = self.data[self.index];
-        self.index += 1;
-        value
-    }
-
-    pub fn as_u16(&mut self) -> u16 {
-        let value = ((self.data[self.index + 1] as u16) << 8) | self.data[self.index] as u16;
-        self.index += 2;
-        value
-    }
-
-    pub fn as_u32(&mut self) -> u32 {
-        let value = (
-               (self.data[self.index + 3] as u32) << 24)
-            | ((self.data[self.index + 2] as u32) << 16)
-            | ((self.data[self.index + 1] as u32) << 8)
-            | self.data[self.index] as u32;
-        self.index += 4;
-        value
-    }
-
-    pub fn as_u64(&mut self) -> u64 {
-        let value = (
-               (self.data[self.index + 7] as u64) << 56)
-            | ((self.data[self.index + 6] as u64) << 48)
-            | ((self.data[self.index + 5] as u64) << 40)
-            | ((self.data[self.index + 4] as u64) << 32)
-            | ((self.data[self.index + 3] as u64) << 24)
-            | ((self.data[self.index + 2] as u64) << 16)
-            | ((self.data[self.index + 1] as u64) << 8)
-            | self.data[self.index] as u64;
-        self.index += 8;
-        value
-    }
-
-    fn parse_size(&mut self, bit_mode: BitMode) -> Size {
-        let address = match bit_mode {
-            BitMode::_32 => Size::u32(self.as_u32()),
-            BitMode::_64 => Size::u64(self.as_u64()),
-        };
-        address
-    }
-
-    pub fn set_index(&mut self, index: usize) {
-        self.index = index;
-    }
-
-    pub fn read_remaining(&mut self) -> &'a[u8] {
-        &self.data[self.index..]
-    }
-}
-
 impl ElfProgramHeader {
     pub fn parse(&mut self, bit_mode: BitMode, reader: & mut BufferReader) {
         self.segment = unsafe { core::mem::transmute::<u32, Segment>(reader.as_u32()) };
@@ -350,11 +255,6 @@ impl ElfHeader {
         self.e_shstrndx = reader.as_u16();
     }
 }
-
-unsafe fn str_from_null_terminated_utf8(s: &[u8]) -> &str {
-    CStr::from_ptr(s.as_ptr() as *const _).to_str().unwrap()
-}
-
 
 impl<'a> ElfFormat<'a> {
     pub fn parse(contents: &'a [u8]) -> Self {
---
+++ src/format/mach_o.rs
@@ -0,0 +1,122 @@
+use core::str;
+use std::{ffi::CStr, fs};
+
+use super::{BitMode, BufferReader, Endianness};
+
+#[repr(u32)]
+#[derive(Debug, Default)]
+pub enum CpuType {
+    #[default]
+    VAX = 0x00000001,
+    ROMP = 0x00000002,
+    NS32032 = 0x00000004,
+    NS32332 = 0x00000005,
+    MC680x0 = 0x00000006,
+    x86 = 0x00000007,
+    MIPS = 0x00000008,
+    NS32352 = 0x00000009,
+    MC98000 = 0x0000000A,
+    HP_PA = 0x0000000B,
+    ARM = 0x0000000C,
+    MC88000 = 0x0000000D,
+    SPARC = 0x0000000E,
+    i860_big_endian = 0x0000000F,
+    i860_little_endian = 0x00000010,
+    RS_6000 = 0x00000011,
+    PowerPC = 0x00000012,
+}
+
+#[repr(u32)]
+#[derive(Debug, Default)]
+pub enum FileType {
+    #[default]
+    object_file = 0x00000001,
+    executable_file = 0x00000002,
+    shared_library_file = 0x00000003,
+    core_file = 0x00000004,
+    preloaded_executable_file = 0x00000005,
+    dynamically_shared_library_file = 0x00000006,
+    dynamic_link_editor = 0x00000007,
+    dynamically_bound_bundle_file = 0x00000008,
+    shared_library_stub = 0x00000009,
+    with_only_debug_sections = 0x0000000A,
+    x86_64_kexts = 0x0000000B,
+    composed_file = 0x0000000C,
+
+}
+
+#[derive(Debug, Default)]
+pub struct LoadCommand {
+    pub command_type: u32,
+    pub command_size: u32,
+}
+
+#[derive(Debug, Default)]
+pub struct MachOHeader {
+    pub bit_mode: BitMode,
+    pub magic_number: u32,
+    pub cpu_type: CpuType,
+    pub cpu_subtype: u32,
+    pub file_type: FileType,
+    pub number_of_load_commands: u32,
+    pub size_of_load_commands: u32,
+    pub flags: u32,
+
+    pub load_commands: Vec<LoadCommand>
+}
+
+
+impl MachOHeader {
+    pub fn build(&self) -> Vec<u8> {
+        Vec::new()
+    }
+
+    pub fn parse(&mut self, reader: &mut BufferReader) {
+        self.magic_number = reader.as_u32();
+        self.bit_mode = match self.magic_number {
+            0xfeedface => BitMode::_32,
+            0xfeedfacf => BitMode::_64,
+            _ => panic!("Unknown format")
+        };
+        self.cpu_type = unsafe { core::mem::transmute::<u32, CpuType>((reader.as_u32() << 8) >> 8) };
+        self.cpu_subtype = reader.as_u32();
+        self.file_type = unsafe { core::mem::transmute::<u32, FileType>(reader.as_u32()) };
+        self.number_of_load_commands = reader.as_u32();
+        self.size_of_load_commands = reader.as_u32();
+        self.flags = reader.as_u32();
+
+        if self.bit_mode == BitMode::_64 {
+            reader.as_u32();
+        }
+
+        for _ in 0..self.number_of_load_commands {
+            self.load_commands.push(LoadCommand {
+                command_type: reader.as_u32(),
+                command_size: reader.as_u32()
+            })
+        }
+    }
+}
+
+#[derive(Debug, Default)]
+pub struct MachOFormat {
+    pub header: MachOHeader
+}
+
+impl MachOFormat {
+    pub fn parse(contents: &[u8]) -> Self {
+        let mut header = MachOHeader::default();
+        let mut reader = BufferReader::new(contents);
+
+        header.parse(&mut reader);
+
+        Self { header }
+    }
+}
+
+pub fn parse(filename: &str) {
+    let contents = fs::read(filename).expect("Should have been able to read the file");
+
+    let elf = MachOFormat::parse(&contents);
+    println!("Elf :{:#?}", elf);
+}
