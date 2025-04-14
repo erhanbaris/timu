1c7e007f6ffeefa1261dc81dd5de429a44769238
Erhan BARIS <erhanbaris@gmail.com>
2025-04-14 21:10:08 +0000

    Add SourceFile struct and update parser to use it

---
+++ src/file.rs
@@ -0,0 +1,21 @@
+use std::path::PathBuf;
+
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+pub struct SourceFile<'a> {
+    path: PathBuf,
+    code: &'a str,
+}
+
+impl<'a> SourceFile<'a> {
+    pub fn new(path: PathBuf, code: &'a str) -> Self {
+        Self { path, code }
+    }
+
+    pub fn path(&self) -> &PathBuf {
+        &self.path
+    }
+
+    pub fn code(&self) -> &'a str {
+        &self.code
+    }
+}
