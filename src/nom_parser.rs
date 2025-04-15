c10412cc8160ad307656177a9c60ce8b33fe0abc
Erhan BARIS <erhanbaris@gmail.com>
2025-04-15 23:24:13 +0200

    Enhance parser with class and field definitions, improve error reporting, and add unit tests for type name parsing

--- src/nom_parser.rs
+++ src/nom_parser.rs
@@ -1,9 +1,10 @@
+use nom::Finish;
 use nom::branch::alt;
 use nom::bytes::complete::{take_till, take_until};
-use nom::character::complete::char;
-use nom::combinator::{map, opt};
-use nom::multi::separated_list0;
-use nom::sequence::{preceded, separated_pair, tuple};
+use nom::character::complete::{alphanumeric1, char};
+use nom::combinator::{all_consuming, complete, consumed, map, opt};
+use nom::multi::{many0, separated_list0};
+use nom::sequence::{preceded, separated_pair, terminated, tuple};
 use nom::{IResult, Parser, character::complete::multispace0, error::ParseError, sequence::delimited};
 
 use nom::{
@@ -14,11 +15,7 @@ use nom::{
 use crate::span::Spanned;
 use nom_locate::{LocatedSpan, position};
 
-type Span<'a> = LocatedSpan<&'a str>;
-
-struct Token<'a> {
-    pub position: Span<'a>,
-}
+pub type Span<'a> = LocatedSpan<&'a str>;
 
 fn cleanup_whitespaces(c: char) -> bool {
     (c == ' ' || c == '\t' || c == '\r' || c == '\n')
@@ -30,71 +27,111 @@ fn cleanup<'a, O, E: ParseError<LocatedSpan<&'a str>>, F: Parser<LocatedSpan<&'a
     delimited(multispace0, f, multispace0)
 }
 
-pub fn parse(input: Span) -> IResult<Span, TimuFileStatementAst> {
-    let (input, _) = cleanup(tag("type")).parse(input)?;
-    let (input, position) = cleanup(position).parse(input)?;
-    let (input, name) = cleanup(alt((take_till(cleanup_whitespaces), take_until("{")))).parse(input)?;
+pub fn parse_type_name<'a>(input: Span<'a>) -> IResult<Span<'a>, TypeName<'a>> {
+    let (input, nullable) = cleanup(opt(char('?'))).parse(input)?;
+    let (input, names) = map(separated_list0(char('.'), cleanup(alphanumeric1)), |items| items).parse(input)?;
+    Ok((
+        input,
+        TypeName {
+            nullable: nullable.is_some(),
+            names,
+        },
+    ))
+}
 
-    let (input, fields) = map(
-        delimited(
-            char('{'),
-            cleanup(separated_list0(
-                char(','),
-                (
-                    cleanup(opt(tag("pub"))),
-                    cleanup(alt((take_till(cleanup_whitespaces), take_until(":")))),
-                    cleanup(char(':')),
-                    cleanup(opt(char('?'))),
-                    cleanup(alt((take_till(cleanup_whitespaces), take_until("}"), take_until(",")))),
-                ),
-            )),
-            char('}'),
-        ),
-        |items| items,
-    )
-    .parse(input)?;
-
-    let mut values = Vec::new();
-    for (is_public, field_name, _, is_nullable, field_type) in &fields {
-        values.push(TypeDefinitionField::TypeDefinition(TypeDefinitionVariable {
-            is_public: is_public.is_some(),
-            name: field_name.fragment().to_string(),
-            is_nullable: is_nullable.is_some(),
-            type_name: field_type.fragment().to_string(),
-        }));
-    }
-
-    println!("input: {}", input.is_empty());
+pub fn parse_field(input: Span) -> IResult<Span, ClassField> {
+    let (input, (is_public, field_name, field_type)) =
+        (cleanup(opt(tag("pub"))), cleanup(terminated(alphanumeric1, cleanup(char(':')))), cleanup(parse_type_name)).parse(input)?;
 
     Ok((
         input,
-        TimuFileStatementAst::TypeDefinition(TypeDefinition {
-            name: String::from(name.to_string()),
+        ClassField {
+            is_public: is_public.is_some(),
+            name: field_name.fragment(),
+            field_type,
+        },
+    ))
+}
+
+pub fn parse_class(input: Span) -> IResult<Span, TimuFileStatementAst> {
+    let (input, _) = cleanup(tag("class")).parse(input)?;
+    let (input, position) = cleanup(position).parse(input)?;
+    let (input, name) = cleanup(alt((take_till(cleanup_whitespaces), take_until("{")))).parse(input)?;
+    let (input, fields) = map(delimited(char('{'), cleanup(separated_list0(char(','), parse_field)), char('}')), |items| items).parse(input)?;
+
+    let mut values = Vec::new();
+    for field in fields.into_iter() {
+        values.push(ClassDefinitionField::ClassField(field));
+    }
+
+    Ok((
+        input,
+        TimuFileStatementAst::ClassDefinition(ClassDefinition {
+            name: name.fragment(),
             values,
         }),
     ))
 }
 
-#[derive(Debug)]
-pub enum TimuFileStatementAst {
-    TypeDefinition(TypeDefinition),
+pub fn parse(input: Span) -> IResult<Span, Vec<TimuFileStatementAst>> {
+    let (input, items) = all_consuming(many0(alt((parse_class,)))).parse(input)?;
+    Ok((input, items))
 }
 
 #[derive(Debug)]
-pub struct TypeDefinition {
-    name: String,
-    values: Vec<TypeDefinitionField>,
+pub struct TypeName<'a> {
+    pub nullable: bool,
+    pub names: Vec<Span<'a>>,
 }
 
 #[derive(Debug)]
-pub enum TypeDefinitionField {
-    TypeDefinition(TypeDefinitionVariable),
+pub enum TimuFileStatementAst<'a> {
+    ClassDefinition(ClassDefinition<'a>),
 }
 
 #[derive(Debug)]
-pub struct TypeDefinitionVariable {
+pub struct ClassDefinition<'a> {
+    name: &'a str,
+    values: Vec<ClassDefinitionField<'a>>,
+}
+
+#[derive(Debug)]
+pub enum ClassDefinitionField<'a> {
+    ClassField(ClassField<'a>),
+}
+
+#[derive(Debug)]
+pub struct ClassField<'a> {
     is_public: bool,
-    name: String,
-    is_nullable: bool,
-    type_name: String,
+    name: &'a str,
+    field_type: TypeName<'a>,
+}
+
+#[cfg(test)]
+mod tests {
+    use std::vec;
+
+    use rstest::rstest;
+
+    use super::{Span, parse_type_name};
+
+    #[rstest]
+    #[case("string", false, vec!["string"])]
+    #[case(" string ", false, vec!["string"])]
+    #[case("string.base", false, vec!["string", "base"])]
+    #[case("string.base . test", false, vec!["string", "base", "test"])]
+    #[case(" string   .        base        . test", false, vec!["string", "base", "test"])]
+    #[case(" ? string   .        base        . test", true, vec!["string", "base", "test"])]
+    #[case("?string", true, vec!["string"])]
+    fn parse_type_name_test<'a>(#[case] code: &'a str, #[case] nullable: bool, #[case] expected: Vec<&str>) {
+        let input = Span::new(code);
+        let result = parse_type_name(input);
+        assert!(result.is_ok(), "Failed to parse type name: {:?}", result.err());
+        let (_, parsed) = result.unwrap();
+
+        assert_eq!(parsed.nullable, nullable, "nullable info does not match expected");
+
+        let parsed: Vec<_> = parsed.names.into_iter().map(|s| s.fragment().to_string()).collect();
+        assert_eq!(parsed, expected, "Parsed type name does not match expected");
+    }
 }
