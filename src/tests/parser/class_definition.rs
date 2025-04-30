a435a0f8aded601b77840bcf255a92cbe4c41dca
Ruslan Asenov <ruslan.asenov@motorolasolutions.com>
2025-04-30 12:13:23 +0200

    Update class_definition tests: modify test case to include parameters in function call for improved coverage

--- src/tests/parser/class_definition.rs
+++ src/tests/parser/class_definition.rs
@@ -25,7 +25,7 @@ use crate::{file::SourceFile, nom_tools::State};
     "class Myclass {a: ?string.base;func init(): MyType {}func init(): MyType {}}"
 )]
 #[case("class Myclass { func init(): MyType { var a = 1;} }", "class Myclass {func init(): MyType {var a = 1;}}")]
-#[case("class Myclass { func init(): MyType { var a = test();} func test(): MyType {} }", "class Myclass {func init(): MyType {var a = test();}func test(): MyType {}}")]
+#[case("class Myclass { func init(): MyType { var a = test(1,2,3);} func test(): MyType {} }", "class Myclass {func init(): MyType {var a = test(1, 2, 3);}func test(): MyType {}}")]
 #[case("class Myclass { func init(): MyType { var a = 1; var b = &a;} }", "class Myclass {func init(): MyType {var a = 1; var b = &a;}}")]
 #[case("class Myclass { func init(a:a, b:b): MyType { var a = 1; var b = &a; a = 20;} }", "class Myclass {func init(a: a, b: b): MyType {var a = 1; var b = &a; a = 20;}}")]
 fn custom_class_test<'a>(#[case] code: &'a str, #[case] expected: &'a str) {
