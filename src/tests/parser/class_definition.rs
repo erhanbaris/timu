d9eb7b67238724a666c0977a906ed8536c4295a8
Erhan BARIS <erhanbaris@gmail.com>
2025-05-02 21:51:48 +0000

    Update dependencies and refactor Tir module: replace 'thiserror' with 'snafu', simplify FunctionCallAst struct, and enhance TirContext for module retrieval.

--- src/tests/parser/class_definition.rs
+++ src/tests/parser/class_definition.rs
@@ -25,9 +25,15 @@ use crate::{file::SourceFile, nom_tools::State};
     "class Myclass {a: ?string.base;func init(): MyType {}func init(): MyType {}}"
 )]
 #[case("class Myclass { func init(): MyType { var a = 1;test();test1.test2();} }", "class Myclass {func init(): MyType {var a = 1; test(); test1.test2();}}")]
-#[case("class Myclass { func init(): MyType { var a = test(1,2,3);} func test(): MyType {} }", "class Myclass {func init(): MyType {var a = test(1, 2, 3);}func test(): MyType {}}")]
+#[case(
+    "class Myclass { func init(): MyType { var a = test(1,2,3);} func test(): MyType {} }",
+    "class Myclass {func init(): MyType {var a = test(1, 2, 3);}func test(): MyType {}}"
+)]
 #[case("class Myclass { func init(): MyType { var a = 1; var b = &a;} }", "class Myclass {func init(): MyType {var a = 1; var b = &a;}}")]
-#[case("class Myclass { func init(a:a, b:b): MyType { var a = 1; var b = &a; a = 20;} }", "class Myclass {func init(a: a, b: b): MyType {var a = 1; var b = &a; a = 20;}}")]
+#[case(
+    "class Myclass { func init(a:a, b:b): MyType { var a = 1; var b = &a; a = 20;} }",
+    "class Myclass {func init(a: a, b: b): MyType {var a = 1; var b = &a; a = 20;}}"
+)]
 #[case(
     "class Myclass { func init(): MyType {if (true || false) {} else if false {} else if false {} else if false {} else {}} }",
     "class Myclass {func init(): MyType {if (true || false) {} else if false {} else if false {} else if false {} else {}}}"
--- src/tir/mod.rs
+++ src/tir/mod.rs
@@ -1,36 +1,20 @@
-use thiserror::Error;
+use context::TirContext;
+use snafu::Snafu;
 
-use crate::{ast::{FileAst, UseAst}, nom_tools::Span};
+use crate::{
+    ast::{FileAst, UseAst},
+    nom_tools::Span,
+};
+
+mod context;
 
 static STANDART_LIBRARY_PREFIX: &str = "std";
 
-pub struct Tir;
-
-#[derive(Debug, Error)]
+#[derive(Debug, Snafu)]
 pub enum TirError {
-
-}
-
-#[derive(Debug, Default)]
-struct TirContext<'a> {
-    modules: Vec<ProjectModule<'a>>,
-}
-
-
-impl<'a> TirContext<'a> {
-    pub fn get_module(&self, use_item: &UseAst<'a>) -> Option<&ProjectModule<'a>> {
-        for module in self.modules.iter() {
-            if let Some(module) = module.get_module(use_item) {
-                return Some(module);
-            }
-        } 
-
-        None
+    #[snafu(visibility(pub), display("data store disconnected"))]
+    ModuleNotFound {
     }
-}
-
-#[derive(Debug, Default)]
-struct File {
 }
 
 #[derive(Debug)]
@@ -40,6 +24,7 @@ struct ProjectModule<'a> {
 }
 
 impl<'a> ProjectModule<'a> {
+    #[cfg(test)]
     fn new(name: Span<'a>) -> Self {
         Self {
             name,
@@ -47,27 +32,28 @@ impl<'a> ProjectModule<'a> {
         }
     }
 
-    fn get_module(&self, search: &UseAst<'a>) -> Option<&ProjectModule<'a>> {
+    fn get_module(&self, path: &[Span<'a>]) -> Option<&ProjectModule<'a>> {
+        if self.name.fragment() != path[0].fragment() {
+            return None;
+        }
 
-        let mut found_module = self.modules
-            .iter()
-            .find(|module| module.name.fragment() == search.paths[0].fragment())?;
+        if path.len() == 1 && self.modules.len() == path.len() {
+            return Some(self);
+        }
 
-        if search.paths.len() > 1 {
-            
-        for path in search.paths[1..].iter() {
-            found_module = found_module.modules
-                    .iter()
-                    .find(|module| module.name.fragment() == path.fragment())?;
+        let mut found_module = self.modules.iter().find(|module| module.name.fragment() == path[1].fragment())?;
+
+        if path.len() > 1 {
+            for path in path[2..].iter() {
+                found_module = found_module.modules.iter().find(|module| module.name.fragment() == path.fragment())?;
             }
         }
-        
+
         Some(found_module)
     }
 }
 
 pub fn build(files: Vec<FileAst<'_>>) -> Result<(), TirError> {
-
     let mut context = TirContext::default();
 
     for file in files.into_iter() {
@@ -88,15 +74,87 @@ fn build_file(context: &mut TirContext, file_ast: FileAst<'_>) -> Result<(), Tir
 }
 
 fn build_use(context: &mut TirContext, use_item: &UseAst<'_>) -> Result<(), TirError> {
-    if let Some(module) = context.get_module(use_item) {
-
-    }
-    
-
-    if *use_item.paths[0].fragment() == STANDART_LIBRARY_PREFIX {
-        println!("Use std library: {}", use_item);
+    if let Some(module) = context.get_module(&use_item.paths) {
+        println!("Module found: {}", module.name);
+        if *use_item.paths[0].fragment() == STANDART_LIBRARY_PREFIX {
+            println!("Use std library: {}", use_item);
+        } else {
+            println!("Use custom library: {}", use_item);
+        }
     } else {
-        println!("Use custom library: {}", use_item);
+        println!("Module not found: {}", use_item);
+
+        //let location = LocatedSpan::new_from_raw_offset(use_item.paths[0].location_offset(), use_item.paths[0].location_line(), (), ());
+        return Err(TirError::ModuleNotFound {
+        });
     }
+
     Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use std::rc::Rc;
+
+    use crate::{
+        file::SourceFile,
+        nom_tools::{Span, State},
+    };
+
+    use super::ProjectModule;
+
+    #[test]
+    fn find_module_test_1<'a>() {
+        let source_file = Rc::new(SourceFile::new("<memory>".into(), ""));
+
+        let state = State {
+            file: source_file.clone(),
+        };
+        let mut module1 = ProjectModule::new(Span::new_extra("test1", state.clone()));
+        let mut module2 = ProjectModule::new(Span::new_extra("test2", state.clone()));
+        let module3 = ProjectModule::new(Span::new_extra("test3", state.clone()));
+
+        module2.modules.push(module3);
+        module1.modules.push(module2);
+
+        let mut context = super::TirContext::default();
+        context.modules.push(module1);
+
+        let found_module =
+            context.get_module(&[Span::new_extra("test1", state.clone()), Span::new_extra("test2", state.clone()), Span::new_extra("test3", state.clone())]);
+        let found_module = found_module.unwrap();
+        assert_eq!(found_module.name.fragment(), &"test3");
+
+        let found_module = context.get_module(&[Span::new_extra("test1", state.clone()), Span::new_extra("test2", state.clone())]);
+        let found_module = found_module.unwrap();
+        assert_eq!(found_module.name.fragment(), &"test2");
+
+        let found_module = context.get_module(&[Span::new_extra("test1", state.clone())]);
+        let found_module = found_module.unwrap();
+        assert_eq!(found_module.name.fragment(), &"test1");
+
+        let found_module = context.get_module(&[Span::new_extra("", state.clone())]);
+        assert_eq!(found_module.is_none(), true);
+    }
+
+    #[test]
+    fn module_not_found<'a>() {
+        let source_file = Rc::new(SourceFile::new("<memory>".into(), ""));
+
+        let state = State {
+            file: source_file.clone(),
+        };
+        let mut module1 = ProjectModule::new(Span::new_extra("test1", state.clone()));
+        let mut module2 = ProjectModule::new(Span::new_extra("test2", state.clone()));
+        let module3 = ProjectModule::new(Span::new_extra("test3", state.clone()));
+
+        module2.modules.push(module3);
+        module1.modules.push(module2);
+
+        let mut context = super::TirContext::default();
+        context.modules.push(module1);
+
+        let found_module = context.get_module(&[Span::new_extra("abc", state.clone())]);
+        assert_eq!(found_module.is_none(), true);
+    }
 }
